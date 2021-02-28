a31e12eb0d005ec13fd13b84e39df15e7e0b640d
Esteban Acosta <estebanacosta19@gmail.com>
2021-02-28 05:57:47 -0500

    completed parse molecule.py

--- PythonCodeWars/Level5_Problems/Parse_Molecule.py
+++ PythonCodeWars/Level5_Problems/Parse_Molecule.py
@@ -1,38 +1,68 @@
 import re
 singleElementTokens = ("^[A-Z][a-z][0-9]+" , "^[A-Z][a-z]", "^[A-Z][0-9]+", "^[A-Z]")
-multipleElementsTokens = ("^\[.+\][0-9]+", "^\[.+\]", "^\(.+\)[0-9]+", "^\(.+\)", "^\{.+\}[0-9]+", "^\{.+\}", )
+multipleElementsTokens = ("^\[[A-Za-z0-9\(\)]+\][0-9]+", "^\[[A-Za-z0-9\(\)]+\]",
+                          "^\([A-Za-z0-9]+\)[0-9]+", "^\([A-Za-z0-9]+\)", 
+                         "^\{.+\}[0-9]+", "^\{.+\}")
 tokens = singleElementTokens + multipleElementsTokens
 
 def condenseFormula(token,substring, formula):
-    end = 0
-    print(substring)
-    
-    elementGroupAndNum = re.split("([0-9]+)",substring)
-    
-    elementNum = 1
-    
-    if len(elementGroupAndNum) > 1:
-         elementNum = int(elementGroupAndNum[1])
-    
+    chemGroupNum = 1
     endBracket = 0
+    typeOfEndingBracket = ""
+    chemGroup = ""
     
-    for i in range(len(substring)):
-        if substring[i] == ")" or substring[i] == "]" or substring[i]  == "}":
-            endBracket = i
-            break
+    if "\[" in token:
+        typeOfEndingBracket = "]"
+    elif "\(" in token:
+        typeOfEndingBracket = ")"
+    elif "\{" in token:
+        typeOfEndingBracket = "}"
     
+    endBracket = substring.index(typeOfEndingBracket)
+
+    #Find any numerical values that come directly after the first occurance of the ending bracket
+    m = re.match("^[0-9]+",substring[endBracket+1:])
+    
+   #if there is a match
+    if m:
+      #store the value
+      chemGroupNum = int(m.group())
+
+    #Make the substring smaller by cutting off the parenthesis that surround the chemical group
     substring = substring[1:endBracket]
     
-    print(substring)
-            
-   
-    #return substring + formula
+    #loop through the chemical group                
+    while len(substring) > 0:
+        #loop through the tokens
+        for token in tokens:
+            #if there is a match between the token and the beginning of the substring
+            match = re.match(token,substring)
+            if match:
+                if token in singleElementTokens:
+                    #take apart the match by splitting it on the number of molecules
+                    elementNameAndNum = re.split("([0-9]+)",match.group())
+                
+                    #if the match includes the element name and the # of molecules
+                    if len(elementNameAndNum) > 1:
+                        #add the element name, multiply the num of molecules outside of the brackets with 
+                        #the num of molecules of the element and that to the string
+                        chemGroup += elementNameAndNum[0] + str(chemGroupNum * int(elementNameAndNum[1]))
+                    #if the match includes just the element name
+                    else:
+                        #add the element name and the number of molecules outside of the parenthesis
+                        chemGroup += elementNameAndNum[0]  + str(chemGroupNum)
+                    substring = substring[len(match.group()):]
+                elif token in multipleElementsTokens:
+                    substring = condenseFormula(token,substring[:len(match.group())],substring[len(match.group()):])
+                break
+    return chemGroup + formula
     
 
 def parse_molecule (formula):   
     elements = {}
     count = 0
     i = 0
+    print(formula)
     #continue looping until the formula is empty
     while len(formula) > 0:
         #loop through each token in the list of tokens
@@ -44,7 +74,7 @@ def parse_molecule (formula):
                 if tokens[i] in singleElementTokens:          
                     #Take the match and split it based on the number
                     elementNameAndNum = re.split("([0-9]+)",match.group())
-                    
+        
                     #if there is a number at the end of the match
                      #and the element is in the list of elements   
                     if len(elementNameAndNum) > 1 and elementNameAndNum[0] in elements:
@@ -84,4 +114,3 @@ def parse_molecule (formula):
                     formula = condenseFormula(tokens[i],formula[:len(match.group())] , formula[len(match.group()):])
                 break
     return elements
- 

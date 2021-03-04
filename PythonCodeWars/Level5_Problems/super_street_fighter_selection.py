b98b0633257286e851404e2d6a0b6f8501488e3c
Esteban Acosta <estebanacosta19@gmail.com>
2021-03-03 22:27:55 -0500

    added two challange files

--- PythonCodeWars/Level5_Problems/super_street_fighter_selection.py
+++ PythonCodeWars/Level5_Problems/super_street_fighter_selection.py
@@ -1,46 +1,126 @@
+#Author: Esteban Acosta
+
+# This time we have to deal with a situation like Super Street Fighter 2 Selection Screen:
+
+# alt text
+
+# As you may see, we now have 16 characters on 3 rows. You might think: let's make an array of 3 arrays! But that's not enough.
+
+# Empty space
+# The first character of the first row (Ryu) is not aligned with the first of the second row (Balrog) but with the second (Ken) and the same goes with the other side; therefore we need to introduce something new, like an offset: the Empty Space.
+
+# The empty space, represented as empty string "", will allow us to keep the grid aligned and rectangular, with spaces that won't be selectable. In this case we need 2 empty spaces (3 rows x 6 columns = 18 slots, 18 slots - 16 characters = 2 empty spaces). Like this:
+
+# |        | Ryu    | E.Honda  | Blanka  | Guile   |         |
+# | Balrog | Ken    | Chun Li  | Zangief | Dhalsim | Sagat   |
+# | Vega   | T.Hawk | Fei Long | Deejay  | Cammy   | M.Bison |
+# The moves of the selection cursor are the same as before: rotate horizontally but stop vertically. When you find empty spaces (1 or more) you need to skip them if you approach them horizontally and get to the next selectable slot with the next fighter on the left or right; and if you approach them vertically you need to just stop and stay where you are.
+
+# Example: if you are on Ryu and move left, you must get to Guile; if you are on Balrog and move up, you must stay on Balrog.
+
+# Notice: I might put empty spaces right in the middle and the rectangular grids can be any size, not only 3x6, deal with this too.
+
+# What's new
+# So, let's resume what are the new issues in this harder version of the Kata:
+
+# The initial position might be any non-empty slot in the grid (given as input).
+# The characters grid (also given as input) might have any rectangular layout, not only 3 rows.
+# The grid might contain empty spaces, both on the borders or right in the middle.
+# Input
+# Fighters grid;
+# Initial position;
+# List of moves.
+# The third input parameter is still the list of moves (all valid ones: left, right, up or down).
+
+# Output
+# The output is the same as before: the list of characters that have been hovered by the selection cursor after each move, successful or not.
+
+# Hopefully test cases will complete my explanation.
+
 def super_street_fighter_selection(fighters, position, moves):
     position = list(position)
-    print(position)
     characters = []
-    print(moves)
     for move in moves:
-        if move == "up":
-            if position[0] <= len(fighters) and position[0] != 0:
-                while fighters[position[0] - 1][position[1]] != " ":
-                      position[0]-=1
-            
-        elif move == "down":
-            if position[0] >= 0 and position[0] != len(fighters) - 1:
-                 if fighters[position[0] + 1][position[1]] != "":
+        #if the move is up and the current position isn't on the first row
+        if move == "up" and position[0] != 0:
+            #if the next position in the grid doesn't have an empty space
+            if fighters[position[0] - 1][position[1]] != "":
+                #move up
+                position[0]-=1
+                
+        #if the move is down and the current position isn't on the last row     
+        elif move == "down" and position[0] != len(fighters) - 1:
+                #if the next position in the grid doesn't have an empty space
+                if fighters[position[0] + 1][position[1]] != "":
+                    #move down
                     position[0]+=1
-                    
+                             
         elif move == "right":
+            #if the next position in the grid is the last position on that row
             if position[1] == (len(fighters[position[0]]) - 1):
+                #if the first position on that same row doesn't have an empty space
                 if fighters[position[0]][0] != "":
                     position[1] = 0
+                #if the first position on that same row does have an empty space
                 else:
-                    position[1] = 1
+                    currentPos =  1
+                    #continue looping until the current position doesn't have an empty space
+                    while currentPos + 1 <= len(fighters[position[0]]) - 1 and fighters[position[0]][currentPos] == "":
+                        #move right
+                        currentPos+=1
+                    #if there is fighter's name in that position
+                    if fighters[position[0]][currentPos] != "":
+                        position[1] = currentPos
+            #if the next position in the grid isn't the last position on that row             
             else:
+                #if the next position ro right doesn't have an empty space
                 if fighters[position[0]][position[1]+1] != "":
                     position[1]+=1
+                #if the next position ro right has an empty space    
                 else:
-                    if position[1] + 2 <= len(fighters[position[0]])-1:
-                        position[1]+=2
-                    else:
-                        position[1] = 0
-        else:
+                    currentPos = position[1] + 1
+                    #continue looping until the current position doesn't have an empty space
+                    while fighters[position[0]][currentPos] == "":
+                        currentPos+=1
+                        #if the current position is outside the bounds of the grid, set it to 0
+                        if currentPos > len(fighters[position[0]]) - 1:
+                            currentPos = 0
+                    #if there's a fighter's name in that position 
+                    if fighters[position[0]][currentPos] != "":
+                        position[1] = currentPos
+    
+        elif move == "left":
+            #if the next position in the grid is the first position on that row
             if position[1] == 0:
+                #if the last position on that same row doesn't have an empty space
                 if fighters[position[0]][(len(fighters[position[0]]) - 1)] != "":
                     position[1] = (len(fighters[position[0]]) - 1)
+                #if the last position on that same row does have an empty space
                 else:
-                    position[1] = (len(fighters[position[0]]) - 2)
+                    currentPos =  (len(fighters[position[0]]) - 2)
+                    #continue looping until the next position to the left doesn't have an empty space
+                    while currentPos - 1 >= 0 and fighters[position[0]][currentPos] == "":
+                        currentPos-=1
+                   #if there's a fighter's name in that position 
+                    if fighters[position[0]][currentPos] != "":
+                        position[1] = currentPos
+             #if the next position in the grid isn't the first position on that row
             else:
+                #if the next position on the same row doesn't have an empty space
                 if fighters[position[0]][position[1]-1] != "":
                     position[1]-=1
+                #if the next position on the same row does have an empty space
                 else:
-                    if position[1] - 2 >= 0:
-                        position[1]-=2
-                    else:
-                        position[1] = len(fighters[position[0]])-1
-        characters.append(fighters[position[0]][position[1]])     
+                    currentPos = position[1] - 1
+                    #continue looping until the next position to the left doesn't have an empty space
+                    while fighters[position[0]][currentPos] == "":
+                        currentPos-=1
+                        #if the current position is out of bounds to the left of the grid
+                        if currentPos < 0:
+                            #set the current position to the last position of that same row
+                            currentPos = len(fighters[position[0]]) - 1
+                    #if there's a fighter's name in that position 
+                    if fighters[position[0]][currentPos] != "":
+                        position[1] = currentPos
+        characters.append(fighters[position[0]][position[1]]) 
     return characters

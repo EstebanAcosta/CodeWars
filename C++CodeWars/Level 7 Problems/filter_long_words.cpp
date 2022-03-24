2f070ba76ac8a7eaaee91f4503d0630192fd0ed6
Esteban Acosta <estebanacosta19@gmail.com>
2022-03-24 00:21:11 -0400

    Added comments

--- C++CodeWars/Level 7 Problems/filter_long_words.cpp
+++ C++CodeWars/Level 7 Problems/filter_long_words.cpp
@@ -1,8 +1,7 @@
 
 #include <vector>
 #include <string>
-
-
+#include <iostream>
 /***
  * 
  * 
@@ -19,21 +18,37 @@ std::vector<std::string> filter_long_words(const std::string& sentence, int n) {
   std::vector<std::string> filtered;
   std::string word;
   
+  //create two variables: one that holds the number of characters in the original string
+  //create one that stores the length of the original string
   int count = 0, end = sentence.length() - 1;
   
+  //loop through each character in the sentence
+  //make sure to declare the character 'c' as a reference so as to not work with the copy of each character
+  //and since we don't intend on modyifing each character in the original string
+  //make sure to declare the character 'c' as a constant reference.
   for (const auto & c : sentence)
   {
 
+    //if the character is a space
     if(isspace(c))
     {
+      //if the new string that we've been building in the past few iterations
+      //is bigger than whatever n is. Add it to the vector.
       if(word.length() > n ) filtered.push_back(word);
       
+      //clear the word for reuse
       word.clear();
     }
     
+    //if the character isn't a space
     else
     { 
+      //add the character to the new string
       word+=c;
+
+      //if it turns out that the we are at the end of the orignal string
+      //and the length of the newly built string is greater than n
+      //add the new string to the vector.
       if(count == end && word.length() > n ) filtered.push_back(word);
     }
     
@@ -45,8 +60,11 @@ std::vector<std::string> filter_long_words(const std::string& sentence, int n) {
 
 int main()
 {
-  std::string sentence = "a dog told me once that I was not a man";
-  
-	filter_long_words(sentence,3);
+  std::string sentence = "ich habe hunger heute und ich möchte heute Morgen in Berlin wandern ";
+
+	std::vector<std::string> filtered_words = filter_long_words(sentence,5);
+
+  for(const auto & word : filtered_words)
+    std::cout << word << std::endl;
 	return 0;
 }
